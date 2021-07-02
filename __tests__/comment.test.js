362d4b6d9b11b03176b44d1d0f52a1916fe1a544
Matthew Saber <mattsaber21@yahoo.com>
2021-07-02 16:03:06 -0700

    red comments post test

---
+++ __tests__/comment.test.js
@@ -0,0 +1,61 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import UserService from '../lib/services/UserService.js';
+import Comment from '../lib/models/Comment.js';
+import Post from '../lib/models/Post.js';
+
+describe('Comment routes', () => {
+
+  let user = {};
+  let agent;
+  beforeEach(async () => {
+    await setup(pool);
+    agent = request.agent(app);
+
+    user = await UserService.create({
+      username: 'test',
+      password: 'password',
+      profilePhotoUrl: 'string'
+    });
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        username: 'test',
+        password: 'password',
+      });
+
+    await Post.insert({
+      userId: user.id,
+      photoUrl: 'https:placekeanu.com/200/150',
+      caption: 'boom boom!',
+      tags: ['tagA', 'tagB', 'tagC']
+    });
+  });
+  it('create a new comment', async () => {
+
+    // const post2 = await Post.insert({
+    //   userId: user.id,
+    //   photoUrl: 'https:placekeanu.com/200/150',
+    //   caption: 'boom boom!',
+    //   tags: ['tagA', 'tagB', 'tagC']
+    // });
+    const res = await agent
+      .post('/api/v1/comments')
+      .send({
+        userId: user.id,
+        commentBy: user.id,
+        post1: 1,
+        comment: ''
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      userId: user.id,
+      commentBy: user.id,
+      post: 1,
+      comment: ''
+    });
+  });
+});
--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -3,26 +3,16 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import UserService from '../lib/services/UserService.js';
+import Post from '../lib/models/Post.js';
 
 describe('POST routes', () => {
+
   let user = {};
   let agent;
   beforeEach(async () => {
     await setup(pool);
     agent = request.agent(app);
-    // user = await UserService.create({
-    //   username: 'test',
-    //   password: 'password',
-    //   profilePhotoUrl: 'string'
-    // });
-    // await agent
-    //   .post('/api/vi/auth/login')
-    //   .send({
-    //     username: 'test',
-    //     password: 'password',
-    //   });
-  });
-  it('create a new post', async () => {
+
     user = await UserService.create({
       username: 'test',
       password: 'password',
@@ -34,6 +24,8 @@ describe('POST routes', () => {
         username: 'test',
         password: 'password',
       });
+  });
+  it('create a new post', async () => {
     const res = await agent
       .post('/api/v1/posts')
       .send({
@@ -53,14 +45,14 @@ describe('POST routes', () => {
   });
   test('gets all posts', async () => {
     const post1 = await Post.insert({
-      user: '1',
+      userId: user.id,
       photoUrl: 'https:placekeanu.com/200/150',
       caption: 'here he is',
       tags: ['tagA', 'tagB', 'tagC']
     });
 
     const post2 = await Post.insert({
-      user: '1',
+      userId: user.id,
       photoUrl: 'https:placekeanu.com/200/150',
       caption: 'boom boom!',
       tags: ['tagA', 'tagB', 'tagC']
